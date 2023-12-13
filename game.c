5c93f00951daf2d5e50b911f595b1b668074f964
Mina Nabil <minanabil2004@gmail.com>
2023-12-14 00:44:58 +0200

    Update

--- game.c
+++ game.c
@@ -2,18 +2,20 @@
 #include <stdlib.h>
 
 int main() {
-    char A[4][4];
-    for(int i=0;i<4;i++)
-        for(int j=0;j<4;j++)
+    int n,m;
+    scanf("%d*%d",&n,&m);
+    char A[n+2][m+2];
+    for(int i=0;i<n+2;i++)
+        for(int j=0;j<m+2;j++)
             if(i==0)
                 A[i][j]=48+j;
             else if(j==0)
                 A[i][j]=48+i;
             else
                 A[i][j]='.';
-    for(int i=0;i<4;i++){
+    for(int i=0;i<n+2;i++){
         printf("\n");
-        for(int j=0;j<4;j++)
+        for(int j=0;j<m+2;j++)
             printf("%c  ",A[i][j]);
     }
 }
